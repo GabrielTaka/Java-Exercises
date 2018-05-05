15f179e6a14dd8dbb07cec7456af67d237611c39
Gabriel Takahashi <gabriel.vargas01@fatec.sp.gov.br>
2018-05-05 12:09:27 -0300

    Delete Vendedor.java

--- 1° Bimestre/LISTA II/EX06/Vendedor.java
+++
@@ -1,9 +0,0 @@
-package br.com.EX06;
-
-public class Vendedor {
-	
-	public double itensVendidos = 0;
-	public int saldoFixo = 200;
-	public double porcentBruto = 0.09;
-	public double rendimento = 0;
-}
